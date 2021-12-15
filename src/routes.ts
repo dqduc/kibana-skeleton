a52f1156f9f9770b9c4da41a14dec845d599cc25
Davey Holler <daveyholler@hey.com>
2021-12-15 15:13:11 -0800

    Updates nav to match Kibana.

--- src/routes.ts
+++ src/routes.ts
@@ -6,18 +6,30 @@ import Home from "./home/home";
 enum SOLUTIONS {
   KIBANA = "kibana",
   OBSERVABILITY = "observability",
+  ENTERPRISE_SEARCH = "enterprise_search",
+  SECURITY = "security",
 }
 
 export const orderedCategories = [
   {
     id: SOLUTIONS.KIBANA,
     icon: "logoKibana",
-    label: "Kibana",
+    label: "Analytics",
+  },
+  {
+    id: SOLUTIONS.ENTERPRISE_SEARCH,
+    icon: "logoEnterpriseSearch",
+    label: "Enterprise Search",
   },
   {
     id: SOLUTIONS.OBSERVABILITY,
     icon: "logoObservability",
     label: "Observability",
+  },
+  {
+    id: SOLUTIONS.SECURITY,
+    icon: "logoSecurity",
+    label: "Security",
   },
 ];
 
